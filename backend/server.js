2de69d5ddd02a3ce8231361b9dca92049011139f
nirupreddy <102612789+reddynirup@users.noreply.github.com>
2024-10-22 16:40:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,21 +30,21 @@ app.use("/user-api", userApis);
 app.use("/recruiter-api", recruiterApis);
 
 
-// //-----------------deployment---------------------
-// const __dirname1=path.resolve();
-// if(process.env.NODE_ENV==="production"){
-//     app.use(express.static(path.join(__dirname1,'/frontend/build')))
-//     app.get("*",(req,res)=>{
-//         res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
-//     })
-// }
-// else{
-//     app.get("/",(req,res)=>{
-//         res.send("API is running successfully..");
-//     })
-// }
+//-----------------deployment---------------------
+const __dirname1=path.resolve();
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname1,'/frontend/build')))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+    })
+}
+else{
+    app.get("/",(req,res)=>{
+        res.send("API is running successfully..");
+    })
+}
 
-// //-----------------deployment---------------------
+//-----------------deployment---------------------
 
 
 
